57a6cf5a4ea8ec9e65dc0f818594f3c9a3ec385e
עידו <lokayido@gmail.com>
2016-05-20 07:04:58 +0300

    update

--- app/src/main/java/com/example/admin/jerusalert/MainScreen.java
+++ app/src/main/java/com/example/admin/jerusalert/MainScreen.java
@@ -1,23 +1,31 @@
 package com.example.admin.jerusalert;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 
 public class MainScreen extends AppCompatActivity {
 
+    private EditText phoneNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
+
         final Context context = this;
-        Button btn = (Button)findViewById(R.id.report_button);
+        ImageButton btn = (ImageButton) findViewById(R.id.report_button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,13 +35,41 @@ public class MainScreen extends AppCompatActivity {
         });
 
         //other_report_button
-        btn = (Button)findViewById(R.id.other_report_button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        ImageButton btnNear = (ImageButton) findViewById(R.id.other_report_button);
+        btnNear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, MapsActivity.class);
                 startActivity(intent);
             }
         });
+
+
+        //other_report_button
+        btn = (ImageButton) findViewById(R.id.button3);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //pop-up
+                phoneNumber = (EditText)findViewById(R.id.phoneNumber);
+                AlertDialog alertDialog = new AlertDialog.Builder(MainScreen.this).create();
+                alertDialog.setTitle("הכנס מס פלאפון");
+                alertDialog.setMessage("הכנס מס פלאפון");
+
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                Intent intent = new Intent(context, MyReqStatus.class);
+                                startActivity(intent);
+                            }
+                        });
+                alertDialog.show();
+
+
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/admin/jerusalert/plscActivity.java
@@ -0,0 +1,63 @@
+package com.example.admin.jerusalert;
+
+        import android.content.Intent;
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+
+        import java.util.Timer;
+        import java.util.TimerTask;
+        import java.util.logging.Handler;
+
+
+public class plscActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TimerTask task = new TimerTask() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(plscActivity.this, MainActivity.class);
+                startActivity(intent);
+                finishscreen();
+            }
+
+            private void finishscreen() {
+                plscActivity.this.finish();
+            }
+
+        };
+        Timer t = new Timer();
+        t.schedule(task, 5000);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
