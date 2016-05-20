35caba34ad823487fd8effe3fa932a1097a0b58b
Daniel Gurfinkel <daniel.gurfi@gmail.com>
2016-05-20 08:52:59 +0300

    add reportObj file

--- app/src/main/java/com/example/admin/jerusalert/reportObj.java
+++ app/src/main/java/com/example/admin/jerusalert/reportObj.java
@@ -1,29 +1,28 @@
 package com.example.admin.jerusalert;
-
 import java.util.Date;
 
 
 public class ReportObj {
-	public int Id;
-	public String Category;
-	public String Subcategory;
-	public double Location_x;
-	public double Location_y;
-	public String Report_time;
-	public String Report_text;
-	public String Phone;
-	public int Counter;
+    public int Id;
+    public String Category;
+    public String Subcategory;
+    public double Location_x;
+    public double Location_y;
+    public String Report_time;
+    public String Report_text;
+    public String Phone;
+    public int Counter;
 
-	public ReportObj() {
-		Id = 0;
-		Category = "";
-		Subcategory = "";
-		Location_x = 0;
-		Location_y = 0;
-		Report_time = "";
-		Report_text = "";
-		Phone = "";
-		Counter = 0;
-	}
+    public ReportObj() {
+        Id = 0;
+        Category = "";
+        Subcategory = "";
+        Location_x = 0;
+        Location_y = 0;
+        Report_time = "";
+        Report_text = "";
+        Phone = "";
+        Counter = 0;
+    }
 
 }
